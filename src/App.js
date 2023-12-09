6be4c59ca9e3f896c9de8cfecd3b7da3137883b3
Michael Agbo <Michael.Agbo@genesys.com>
2023-12-09 17:01:07 +0000

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: [],
+      book: [],
     };
   }
 
@@ -24,43 +24,44 @@ class App extends Component {
   }
 
   async addClick() {
-    var newBook = document.getElementById("newNotes").value;
+    var newBook = document.getElementById("newBook").value;
     const data = new FormData();
     data.append("newBook", newBook);
 
-    fetch(this.API_URL+"api/bookapp/AddBook", {
-      method:"POST",
-      body:data
-    }).then(res=>res.json())
-    .then((result) => {
-      alert(result);
-      this.refreshBook();
+    fetch(this.API_URL + "api/bookapp/AddBook", {
+      method: "POST",
+      body: data,
     })
+      .then((res) => res.json())
+      .then((result) => {
+        alert(result);
+        this.refreshBook();
+      });
   }
 
-  async deteleClick(id) { 
-
-    fetch(this.API_URL+"fullstackproject/bookapp/DeleteBook?id="+id, {
-      method:"DELETE",
-    }).then(res=>res.json())
-    .then((result) => {
-      alert(result);
-      this.refreshBook();
+  async deteleClick(id) {
+    fetch(this.API_URL + "fullstackproject/bookapp/DeleteBook?id=" + id, {
+      method: "DELETE",
     })
+      .then((res) => res.json())
+      .then((result) => {
+        alert(result);
+        this.refreshBook();
+      });
   }
-  
+
   render() {
-    const { notes } = this.state;
+    const { book } = this.state;
     return (
       <div className="App">
         <h2> Todo App</h2>
         <input id="newBook" />
         &nbsp;
-        <button onClick={() => this.addClick()}>Add Notes</button>
-        {notes.map((note) => (
+        <button onClick={() => this.addClick()}>Add book</button>
+        {book.map((book) => (
           <p>
-            <b> * {note.desciption}</b>
-            <button onClick={() => this.deleteClick(note.id)}>
+            <b>* {book.desciption}</b>&nbsp;
+            <button onClick={() => this.deleteClick(book.id)}>
               delete Book
             </button>
           </p>
