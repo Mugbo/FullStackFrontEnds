5ddffa3bf4a9b7882d7ce9ca44a204926cc43df3
Michael Agbo <Michael.Agbo@genesys.com>
2023-12-09 16:42:10 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  API_URL = "http://http://localhost:3000/";
+
+  componentDidMount() {
+    this.refreshBooks();
+  }
+
+  async refreshBooks() {
+    fetch(this.API_URL + "api/bookapp/GetBook")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ book: data });
+      });
+  }
+
+  async addClick() {
+    var newBook = document.getElementById("newNotes").value;
+    const data = new FormData();
+    data.append("newBook", newBook);
+
+    fetch(this.API_URL+"api/bookapp/AddBook", {
+      method:"POST",
+      body:data
+    }).then(res=>res.json())
+    .then((result) => {
+      alert(result);
+      this.refreshBook();
+    })
+  }
+
+  async deteleClick(id) { 
+
+    fetch(this.API_URL+"fullstackproject/bookapp/DeleteBook?id="+id, {
+      method:"DELETE",
+    }).then(res=>res.json())
+    .then((result) => {
+      alert(result);
+      this.refreshBook();
+    })
+  }
+  
+  render() {
+    const { notes } = this.state;
+    return (
+      <div className="App">
+        <h2> Todo App</h2>
+        <input id="newBook" />
+        &nbsp;
+        <button onClick={() => this.addClick()}>Add Notes</button>
+        {notes.map((note) => (
+          <p>
+            <b> * {note.desciption}</b>
+            <button onClick={() => this.deleteClick(note.id)}>
+              delete Book
+            </button>
+          </p>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
