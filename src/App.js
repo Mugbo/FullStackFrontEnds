fa20a2a464923fcc322410913c5ba3f73740522f
Connor_N <connorngouana@gmail.com>
2023-12-13 11:52:01 +0000

    S

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 // App.js
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./App.css"
+import "./App.css";
 
 function App() {
   const [book, setBook] = useState("");
   const [allBooks, setAllBooks] = useState([]);
 
   useEffect(() => {
-    getAllBooks();
+    axios
+      .get("http://localhost:5001/GetBook")
+      .then((result) => setAllBooks(result.data))
+      .catch((err) => console.log(err));
   }, []);
 
   const addClick = () => {
     axios
       .post("http://localhost:5001/AddBook", { book: book })
       .then((result) => console.log("Book Added", result))
-      .catch((err) => console.log(err));
-    getAllBooks();
-  };
-
-  const getAllBooks = () => {
-    axios
-      .get("http://localhost:5001/GetBook")
-      .then((result) => setAllBooks(result.data))
       .catch((err) => console.log(err));
   };
 
@@ -31,6 +26,16 @@ function App() {
       .delete("http://localhost:5001/DeleteBook/" + id)
       .then((result) => {
         setAllBooks(result.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const UpdateBook = (id) => {
+    axios
+      .put("http://localhost:5001/UpdateBook/" + id)
+      .then((result) => {
+        console.log(result);
+        // Add logic to handle the update response if needed
       })
       .catch((err) => console.log(err));
   };
@@ -51,19 +56,29 @@ function App() {
       </div>
       <div className="all-books-container">
         <h2 className="all-books-heading">All Books</h2>
-        {allBooks.map((book) => (
-          <div key={book._id} className="book-item">
-            <p className="book-title">{book.book}</p>
-            <button
-              type="button"
-              className="delete-button"
-              onClick={() => deleteBook(book._id)}
-            >
-              Delete Book
-            </button>
-          </div>
-        ))}
-        <div></div>
+        {allBooks.length === 0 ? (
+          <div>No records</div>
+        ) : (
+          allBooks.map((book) => (
+            <div className="task" key={book._id}>
+              <div className="checkbox" onClick={() => UpdateBook(book._id)}>
+                <div className="book-item">
+                  <p className="book-title">{book.book}</p>
+                </div>
+              </div>
+              <button
+                type="button"
+                className="delete-button"
+                onClick={() => deleteBook(book._id)}
+              >
+                Delete Book
+              </button>
+            </div>
+          ))
+        )}
+
+        {/* This empty div seems unnecessary */}
+        {/* <div></div> */}
       </div>
     </div>
   );
